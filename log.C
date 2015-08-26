1547b4ebc2865221a66fdf19c3f030892d1e6815
Frank-yujiang <jiangyu@cn.ibm.com>
2015-08-26 17:46:46 +0800

    update log.C from github

--- log.C
+++ log.C
@@ -1,2 +1,3 @@
 v1.0  Create this file                                 2015/8/26
 v1.1  Test update without add                          2015/8/26
+v1.2  Edit this file in github                         2015/8/26
